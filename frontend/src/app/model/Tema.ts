47fea6de77b8b9171b5af8260b137722e6dc04c7
GustavoReis1991 <93771448+GustavoReis1991@users.noreply.github.com>
2022-04-06 17:48:13 -0300

    Delete frontend directory

--- frontend/src/app/model/Tema.ts
+++
@@ -1,9 +0,0 @@
-import { Postagem } from "./Postagem";
-
-export class Tema {
-    public id: number;
-    public descricao: string;
-    public assunto: string;
-    public hashtag: string;
-    public postagem: Postagem[]
-}
